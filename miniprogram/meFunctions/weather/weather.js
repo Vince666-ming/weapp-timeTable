9265ddbee6f513a1f08b01f16f95f888fb5ef640
Vincent <2363659293@qq.com>
2020-02-16 12:31:15 +0800

    Update weather.js

--- miniprogram/meFunctions/weather/weather.js
+++ miniprogram/meFunctions/weather/weather.js
@@ -3,78 +3,60 @@ Component({
   /**
    * 组件的属性列表
    */
-  properties: {
-
-  },
-
   /**
    * 组件的初始数据
    */
   data: {
-
-    weather: [], //实况天气
-    weatherweek: [], //七日天气
+    // location: [],
+    weather: []
   },
 
   /**
    * 组件的方法列表
    */
-  created: function(options) {
+  created: function (options) {
     this.getapi();
     // this.weatherweekday();
   },
   methods: {
-    getapi: function() {
+    getapi: function () {
       var _this = this;
       // 获取IP地址
       wx.request({
-        url: 'https://www.tianqiapi.com/api/?version=v1&cityid=101110101&appid=77118166&appsecret=wQ7NdnTF',
+        url: 'https://ip.tianqiapi.com/',
         data: {},
         method: 'POST',
         header: {
           'content-type': 'application/x-www-form-urlencoded'
         },
-        success: function(res) {
+        success: function (res) {
           console.log(res);
           // 根据IP获取天气数据
+          _this.setData({
+            location: res.data
+          });
           _this.weathertoday(res.data.ip);
-          _this.weatherweekday(res.data.ip)
+          // _this.weatherweekday(res.data.ip)
         }
       });
     },
     // 天气api实况天气
-    weathertoday: function(ip) {
+    weathertoday: function (ip) {
       var _this = this;
       wx.request({
-        url: 'https://www.tianqiapi.com/api/?version=v6&cityid=101110101&appid=77118166&appsecret=wQ7NdnTF',
-        
+        url: 'https://www.tianqiapi.com/api/?version=v1&appid=77118166&appsecret=wQ7NdnTF',
+        data: {
+          'ip': ip
+        },
         method: 'GET',
         header: {
           'content-type': 'application/x-www-form-urlencoded'
         },
-        success: function(res) {
+        success: function (res) {
           _this.setData({
             weather: res.data
           });
-          console.log(_this.data.weather)
-        }
-      });
-    },
-    // 天气api一周天气
-    weatherweekday: function(ip) {
-      var _this = this;
-      wx.request({
-        url: 'https://www.tianqiapi.com/api/?version=v1&cityid=101110101&appid=77118166&appsecret=wQ7NdnTF',
-        
-        method: 'GET',
-        header: {
-          'content-type': 'application/x-www-form-urlencoded'
-        },
-        success: function(res) {
-          _this.setData({
-            weatherweek: res.data
-          });
-          console.log(_this.data.weatherweek)
+          console.log(res.data)
         }
       });
     }
